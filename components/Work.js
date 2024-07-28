e4333d79d907f529fff93ac6e395fff35f231ab8
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-07-28 23:14:05 +0200

    update work & works

--- components/Work.js
+++ components/Work.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/work.module.scss";
+import Sticky from "react-stickynode";
 import { motion } from "framer-motion";
 
 export default function Work({
@@ -18,73 +19,48 @@ export default function Work({
     visible: { scale: 1 },
   };
 
-  useEffect(() => {
-    // console.log("elRef", elRef.current);
-
-    return () => {};
-  }, [elRef]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const el = elRef.current;
-      if (el) {
-        // console.log("elRef", elRef.current);
-        const stickyTop = parseInt(window.getComputedStyle(el).top, 10);
-        const marginTop = parseInt(window.getComputedStyle(el).marginTop, 10);
-        const height = parseInt(window.getComputedStyle(el).height, 10);
-        // console.log("stickyTop", stickyTop);
-        const currentTop = el.getBoundingClientRect().top;
-        console.log("stickyTop", stickyTop);
-        console.log("marginTop", marginTop);
-        console.log("currentTop", currentTop - 10);
-        // console.log("currentTop", currentTop);
-        if (currentTop - 10 * id < stickyTop) {
-          // setIsAnimating(true);
-          // setTimeout(() => {
-          //   setIsAnimating(false);
-          // }, 100);
-          changeVideo(img);
-        } // el.classList.toggle(styles.isSticky, currentTop === stickyTop);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const handleStateChange = (status) => {
+    if (status.status === Sticky.STATUS_FIXED) {
+      changeVideo(id);
+    } else if (status.status === Sticky.STATUS_ORIGINAL) {
+      changeVideo(id);
+    }
+  };
   return (
-    <motion.div
-      className={styles.workContainer}
-      ref={elRef}
-      key={name}
-      // initial="visible"
-      // animate={isAnimating ? "hidden" : "visible"}
-      // variants={variants}
-      // transition={{ duration: 0.2, ease: [0.34, 1.56, 0.64, 1] }}
+    <Sticky
+      onStateChange={handleStateChange}
+      top={700}
+      innerActiveClass="stickyContainer"
+      className="stickyContainer"
     >
-      <div className={styles.work}>
-        {/* <div className={styles.container1}> */}
-        <div className={styles.container}>
-          <div className={styles.logoContainer}>
-            <img src={img?.src} className={styles.logo} alt="Project Logo" />
-            <p className={styles.title}>{name}</p>
-          </div>
+      <motion.div
+        className={styles.workContainer}
+        // initial="visible"
+        // animate={isAnimating ? "hidden" : "visible"}
+        // variants={variants}
+        // transition={{ duration: 0.2, ease: [0.34, 1.56, 0.64, 1] }}
+      >
+        <div className={styles.work}>
+          {/* <div className={styles.container1}> */}
+          {/* <div className={styles.container}> */}
+          {/* <div className={styles.logoContainer}> */}
+          <img src={img?.src} className={styles.logo} alt="Project Logo" />
+          {/* <p className={styles.title}>{name}</p> */}
+          {/* </div> */}
+          {/* </div> */}
         </div>
-      </div>
-      <div className={styles.separator1}></div>
-      <div className={styles.separator2}></div>
-      <div className={styles.container1}>
-        {/* <img
+        <div className={styles.separator1}></div>
+        <div className={styles.separator2}></div>
+        <div className={styles.container1}>
+          {/* <img
           src={overview?.src}
           className={styles.overview}
           alt="Project Image"
           style={{ marginTop: -shift + "%" }}
         /> */}
-      </div>
-      {/* </div> */}
-    </motion.div>
+        </div>
+        {/* </div> */}
+      </motion.div>
+    </Sticky>
   );
 }
--- components/Works.js
+++ components/Works.js
@@ -1,47 +1,31 @@
 import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/work.module.scss";
 import Work from "./Work";
-import { motion } from "framer-motion";
+import { FlatTree, motion } from "framer-motion";
 
-import snake from "../public/images/logos/snake_two.svg";
-import recader from "../public/images/logos/es_recader.svg";
-import kenken from "../public/images/logos/kenken.svg";
-import cards from "../public/images/logos/cards.svg";
-import theogony from "../public/images/logos/theogony.svg";
-import theogonyAPI from "../public/images/logos/theogony_api.svg";
+import logo1 from "../public/images/logos/theogony.svg";
+import logo2 from "../public/images/logos/es_recader.svg";
+import logo3 from "../public/images/logos/theogony_api.svg";
+import logo4 from "../public/images/logos/kenken.svg";
+import logo5 from "../public/images/logos/snake_two.svg";
+import logo6 from "../public/images/logos/cards.svg";
 
-import test1 from "../public/images/test-pic.jpg";
 // import test2 from "../public/images/test-pic-2.jpg";
-import test2 from "../public/images/test-pic-2.png";
+import test1 from "../public/images/test-pic-3.jpg";
+import test2 from "../public/images/test-pic-2.jpg";
 import test3 from "../public/images/test-pic-3.jpg";
-import test4 from "../public/images/test-pic-4.jpg";
-import test5 from "../public/images/test-pic-5.jpg";
-import test6 from "../public/images/test-pic-6.jpg";
+import test4 from "../public/images/test-pic-2.jpg";
+import test5 from "../public/images/test-pic-3.jpg";
+import test6 from "../public/images/test-pic-2.jpg";
+
+// import video from "../public/images/test-video-1.mp4";
+
+const logos = [logo1, logo2, logo3, logo4, logo5, logo6];
+const images = ["Theogony", "test2", "test3", "test4", "test5", "test6"];
+// const logos = [logo1, logo2];
 
 export default function Works() {
   const elRef = useRef(null);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     console.log("elRef", elRef);
-  //     const el = elRef.current;
-  //     if (el) {
-  //       const stickyTop = parseInt(window.getComputedStyle(el).top, 10);
-  //       console.log("stickyTop", stickyTop);
-  //       const currentTop = el.getBoundingClientRect().top;
-  //       console.log("currentTop", currentTop);
-
-  //       el.classList.toggle(styles.isSticky, currentTop === stickyTop);
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   // Cleanup the event listener on component unmount
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
 
   const variants = {
     hidden: { scale: 0.95 },
@@ -51,88 +35,93 @@ export default function Works() {
   const [imageSrc, setImageSrc] = useState(test2?.src);
   const [isAnimating, setIsAnimating] = useState(false);
 
+  var currentImg = undefined;
+
   useEffect(() => {
-    console.log("imageSrc changed", imageSrc);
+    // console.log("imageSrc changed", imageSrc);
 
     return () => {};
   }, [imageSrc]);
 
-  const changeVideo = (img) => {
-    setImageSrc((prevSrc) => {
-      if (prevSrc !== img?.src) {
-        setIsAnimating(true);
-        setTimeout(() => {
-          setImageSrc(img.src);
-          setIsAnimating(false);
-        }, 100); // Duration of the fade-out animation
-      } else {
-        // setImageSrc(test2?.src); // Change back to test2.src without animation
-      }
-      return prevSrc;
-    });
+  const changeVideo = (id) => {
+    // window.getComputedStyle(elRef.current.children[id]).opacity == 1
+    //   ? (elRef.current.children[id].style.opacity = 0)
+    //   : (elRef.current.children[id].style.opacity = 1);
+
+    // elRef.current.children[id].style.opacity = 0;
+    console.log(
+      window.getComputedStyle(elRef.current.children[id].children[0])
+        .clipPath == "circle(5% at 50% 85%)"
+    );
+    // console.log(elRef.current.children[id].children[0]);
+    window.getComputedStyle(elRef.current.children[id]).clipPath ==
+    "circle(0% at 50% 85%)"
+      ? (elRef.current.children[id].style.clipPath = "circle(85%)")
+      : (elRef.current.children[id].style.clipPath = "circle(0% at 50% 85%)");
   };
+
   return (
-    // <div style={{ position: "relative", height: "200vh" }}>
-    <div className={styles.works}>
-      <div className={styles.video}>
-        <motion.img
-          src={imageSrc}
-          className={styles.overview}
-          alt="Project Image"
-          ref={elRef}
-          initial="visible"
-          animate={isAnimating ? "hidden" : "visible"}
-          variants={variants}
-          transition={{ duration: 0.2, ease: [0.34, 1.56, 0.64, 1] }}
-        />
+    <div>
+      <div className={styles.video} ref={elRef}>
+        {/* <div className={styles.overview}> */}
+        <div style={{ zIndex: 1 }} className={styles.workTitle}>
+          {/* <div
+            style={{
+              height: "10rem",
+              width: "100%",
+              backgroundColor: "red",
+              position: "absolute",
+              top: "65%",
+            }}
+          ></div> */}
+          <span className={styles.gradientText}>WORK</span>
+        </div>
+        {/* </div> */}
+        {images.map((image, index) => (
+          <div
+            className={styles.overview}
+            style={{
+              clipPath: "circle(0% at 50% 85%)",
+              backgroundImage: `linear-gradient(${
+                (index + 1) * (index + 1) * 12 + 45
+              }deg, rgba(175,66,97,1) 0%, rgba(0,134,209,1) 100%)`,
+            }}
+          >
+            {/* <video src={video}></video> */}
+            <video muted loop autoPlay>
+              <source
+                src={`/videos/test-video-${index + 1}.mp4`}
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+            <div>
+              <h1 className={styles.title}>{image}</h1>
+            </div>
+            <div className={styles.container}>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Fugiat, molestiae amet! Quam minus sit beatae, neque ea eligendi
+                dolorem eum id iusto, ad praesentium aperiam itaque possimus
+                tempore corporis exercitationem!
+              </p>
+            </div>
+
+            {/* <img
+              priority={true}
+              src={image.src}
+              alt="Project Image"
+              style={{ zIndex: index + 2 }}
+            /> */}
+          </div>
+        ))}
       </div>
-      {/* </div> */}
-      {/* <div className={styles.video2}> */}
-      <Work
-        img={theogony}
-        name={"Theogony"}
-        overview={test1}
-        shift={65}
-        changeVideo={changeVideo}
-        id={2}
-      />
-      <Work
-        img={recader}
-        name="es Recader"
-        overview={test2}
-        shift={10}
-        changeVideo={changeVideo}
-        id={1}
-      />
-      {/* <Work
-        img={theogonyAPI}
-        name="Theogony API"
-        overview={test4}
-        shift={35}
-        changeVideo={changeVideo}
-      />
-      <Work
-        img={kenken}
-        name="Ken Ken"
-        overview={test3}
-        shift={25}
-        changeVideo={changeVideo}
-      />
-      <Work
-        img={snake}
-        name="Snake II"
-        overview={test6}
-        shift={30}
-        changeVideo={changeVideo}
-      />
-      <Work
-        img={cards}
-        name="Card Mat"
-        overview={test5}
-        shift={10}
-        changeVideo={changeVideo}
-      /> */}
-      {/* </div> */}
+      {logos.map((logo, index) => (
+        <div>
+          <Work img={logo} changeVideo={changeVideo} id={index + 1} />
+          <div style={{ height: "50vh" }}></div>
+        </div>
+      ))}
     </div>
   );
 }
