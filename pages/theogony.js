f046aca9c5211b51f7c7df2f795d9f3e7001e702
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-08-25 23:01:24 +0200

    Update theogony.js

--- pages/theogony.js
+++ pages/theogony.js
@@ -1,249 +1,54 @@
-import React, { useRef, useEffect, useState } from "react";
-import styles from "../styles/workInfo.module.scss";
-import classNames from "classnames";
+import React from "react";
 import logo1 from "../public/images/logos/theogony.svg";
-import mockupLg from "../public/images/mockup-lg.png";
-import Image from "next/image";
-import TechPill from "../components/TechPill";
-import { useScroll, useTransform, motion } from "framer-motion";
-import Lenis from "lenis";
-import MouseBall from "../components/MouseBall";
-import { useRouter } from "next/router";
+import nextLogo from "../public/images/logos/es_recader.svg";
+import WorkPage from "../components/WorkPage";
 
-function Theogony() {
-  const router = useRouter();
-  // Lenis
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  const handleTransitionOut = () => {
-    setTransitionIn(true);
-    setShowBall(false);
-    setTimeout(() => {
-      router.push("/theogony2");
-    }, 400);
+function Theogony2() {
+  const work = {
+    title: "Theogony",
+    subtitle: "Greek Gods Family Tree API",
+    logo: logo1,
   };
 
-  const containerRef = useRef(null);
-  const containerTextRef = useRef(null);
-  const [showBall, setShowBall] = useState({ show: false, text: "HOLA" });
-  const [transitionIn, setTransitionIn] = useState(false);
-
-  const { scrollYProgress: scrollYProgressContainer } = useScroll({
-    target: containerRef,
-    offset: ["start end", "end start"],
-  });
-
-  const { scrollYProgress: scrollYProgressText } = useScroll({
-    target: containerTextRef,
-    offset: ["start end", "end start"],
-  });
-
-  const transformTextY = useTransform(scrollYProgressText, [0, 1], [0, 300]);
-  const transformRightY = useTransform(
-    scrollYProgressContainer,
-    [0, 1],
-    [100, -100]
-  );
-  const transformLeftY = useTransform(
-    scrollYProgressContainer,
-    [0, 1],
-    [-100, 100]
-  );
-
-  const textAnimation = {
-    initial: {
-      opacity: 0,
-      y: "100%",
-      transition: {
-        type: "spring",
-        damping: 250,
-        stiffness: 2000,
-        mass: 20,
-      },
-    },
-    animate: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: "spring",
-        damping: 250,
-        stiffness: 2000,
-        mass: 20,
-      },
-    },
-  };
-
-  return (
-    <main className={styles.main}>
-      <MouseBall
-        show={showBall}
-        ballAnimation={false}
-        xStartProp={0}
-        yStartProp={0}
-      />
-      <motion.div className={styles.intro} style={{ y: transformTextY }}>
-        <Image src={logo1} className={styles.logo} alt="Logo" priority />
-        <div className={styles.titleContainer}>
-          <motion.h1
-            className={classNames(styles.title, "gradientText")}
-            variants={textAnimation}
-            initial="initial"
-            animate="animate"
-          >
-            Theogony
-          </motion.h1>
-        </div>
-        <div className={styles.subtitleContainer}>
-          <motion.h2
-            className={styles.subtitle}
-            variants={textAnimation}
-            initial="initial"
-            animate="animate"
-          >
-            GREEK GODS FAMILY TREE API
-          </motion.h2>
-        </div>
-      </motion.div>
-      <section>
-        <motion.p className={styles.overview} ref={containerTextRef}>
-          <i>Theogony API</i> is a REST API that gives you the possibility to
+  const texts = {
+    overview: `Theogony API is a REST API that gives you the possibility to
           retrieve information about Greek gods and the relationships and
           partnerships they had. This way, e.g., you are able to trace the
           entire Greek Gods Family Tree from a child to its great, great,
-          great... grandfather.
-        </motion.p>
-        <div className={styles.workInfo}>
-          {/* <h3>Stack</h3> */}
-          <h3 className={styles.smallTitle}>Role</h3>
-          <h3 className={styles.smallTitle}>Page</h3>
+          great... grandfather.`,
+    role: "Development & Deploy",
+    page: "www.theogonia.net",
+    why: `This is a personal project where I put myself a the challenge to
+          create a public API for the community. As I couldn't find too many
+          playful APIs for people to make a a code project.`,
+  };
 
-          {/* <div className={styles.line}></div> */}
-          <div className={styles.line}></div>
-          <div className={styles.line}></div>
+  const pills = [
+    "TypeScript",
+    "NodeJS",
+    "ExpressJS",
+    "MySQL",
+    "REST",
+    "TypeORM",
+    "AWS",
+    "DBeaver",
+    "CSR Pattern",
+    "Best Practices",
+  ];
 
-          {/* <div className={styles.pills}>
-            <TechPill name="TypeScript" color="#3178C6" />
-            <TechPill name="NodeJS" color="#68A063" />
-            <TechPill name="MySQL" color="#00758F" />
-            <TechPill name="AWS" color="#FF9900" />
-          </div> */}
-          <div className={styles.info}>
-            <p>Development & Deploy</p>
-          </div>
-          <div className={styles.info}>
-            <a
-              href="http://theogonia.net"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              theogonia.net
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.mockupContainer}>
-          <div className={styles.mockupLg}>
-            <Image src={mockupLg} className={styles.mockupLgImage} />
-            <div>
-              <video muted loop autoPlay>
-                <source src={`/videos/output2.mp4`} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            </div>
-          </div>
-        </div>
-        <div className={styles.workInfo}>
-          <h3 className={styles.smallTitle}>Why</h3>
-          <h3 className={styles.smallTitle}>Stack</h3>
-
-          <div className={styles.line}></div>
-          <div className={styles.line}></div>
-
-          <div className={styles.text}>
-            <p>
-              This is a personal project where I put myself a the challenge to
-              create a public API for the community. As I couldn't find too many
-              playful APIs for people to make a a code project.
-            </p>
-            <p style={{ marginTop: "2vh" }}>
-              This is a personal project where I put myself a the challenge to
-              create a public API for the community. As I couldn't find too many
-              playful APIs for people to make a a code project.
-            </p>
-          </div>
-          <div className={styles.pills}>
-            <TechPill name="TypeScript" color="#3178C6" />
-            <TechPill name="NodeJS" color="#68A063" />
-            <TechPill name="ExpressJS" color="#FF9900" />
-            <TechPill name="MySQL" color="#00758F" />
-            <TechPill name="REST" color="#FF9900" />
-            <TechPill name="TypeORM" color="#FF9900" />
-            <TechPill name="AWS" color="#FF9900" />
-            <TechPill name="DBeaver" color="#FF9900" />
-            <TechPill name="CSR Pattern" color="#FF9900" />
-            <TechPill name="Best Practices" color="#FF9900" />
-          </div>
-        </div>
-        <motion.div className={styles.phoneMockups} ref={containerRef}>
-          <motion.div style={{ y: transformRightY }}></motion.div>
-          <motion.div style={{ y: transformLeftY }}></motion.div>
-        </motion.div>
-      </section>
-      <footer className={styles.footer}>
-        <div
-          className={styles.nextCaseContainer}
-          onMouseEnter={() => setShowBall({ show: true, text: "Check" })}
-          onMouseLeave={() => setShowBall({ show: false, text: "" })}
-          onClick={handleTransitionOut}
-        >
-          <div className={styles.nextCaseTitle}>
-            <h3 className={classNames(styles.smallTitle)}>Next Case</h3>
-          </div>
-          <div className={styles.nextContainer}>
-            <div
-              className={classNames(styles.logoNextContainer, {
-                [styles.logoNextContainerTransition]: transitionIn,
-              })}
-            >
-              <Image src={logo1} className={styles.logoNext} alt="Logo" />
-            </div>
-            <div
-              className={styles.backgroundTransition}
-              style={{
-                width: transitionIn ? "150vw" : "6vw",
-                height: transitionIn ? "150vw" : "6vw",
-              }}
-            ></div>
-          </div>
-          <h3 className={styles.nextCase}>es Recader</h3>
-          <div
-            className={styles.line}
-            style={{ position: "absolute", top: "93%" }}
-          ></div>
-          {/* <div className={styles.nextCaseOverview}></div> */}
-
-          {/* <div className={styles.background}></div> */}
-        </div>
-      </footer>
-    </main>
+  const nextWork = {
+    url: "esrecader",
+    logo: nextLogo,
+    title: "Es Recader",
+  };
+  return (
+    <WorkPage
+      work={work}
+      texts={texts}
+      nextWork={nextWork}
+      pills={pills}
+    ></WorkPage>
   );
 }
 
-export default Theogony;
+export default Theogony2;
