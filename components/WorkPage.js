2e5dfccc94a2b6b336a3a3608ce6e5bc7a8e86a7
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-09-03 20:52:08 +0200

    Update WorkPage.js

--- components/WorkPage.js
+++ components/WorkPage.js
@@ -3,7 +3,7 @@ import styles from "../styles/workInfo.module.scss";
 import intro from "../styles/intro.module.scss";
 import classNames from "classnames";
 import logo1 from "../public/images/logos/theogony.svg";
-import mockupLg from "../public/images/mockup-lg.png";
+import mockupLg from "../public/images/mockup-lg-3.png";
 import Image from "next/image";
 import TechPill from "../components/TechPill";
 import { useScroll, useTransform, motion, animate } from "framer-motion";
@@ -13,6 +13,7 @@ import { useRouter } from "next/router";
 import { useCurrentTime } from "../hooks/useCurrentTime";
 import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
 import useWindowSize from "../hooks/useWIndowSize";
+import Head from "next/head";
 
 function WorkPage({ work, texts, video, nextWork, pills }) {
   const { width, height } = useWindowSize();
@@ -66,14 +67,16 @@ function WorkPage({ work, texts, video, nextWork, pills }) {
   };
 
   const handleTransitionOut = () => {
-    setTransitionIn(true);
-    setShowBall(false);
-    // bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-    scrollToBottom();
+    if (nextWork.url !== "snaketwo") {
+      setTransitionIn(true);
+      setShowBall(false);
+      // bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+      scrollToBottom();
 
-    setTimeout(() => {
-      router.push(nextWork.url);
-    }, 400);
+      setTimeout(() => {
+        router.push(nextWork.url);
+      }, 400);
+    }
   };
 
   const handleTransitionHome = () => {
@@ -154,135 +157,132 @@ function WorkPage({ work, texts, video, nextWork, pills }) {
   };
 
   return (
-    <main className={styles.main}>
-      <header
-        className={classNames(intro.header)}
-        style={{ position: "fixed" }}
-      >
-        <a onClick={handleTransitionHome} style={{ color: headerColor }}>
-          Noah Guardiola
-        </a>
-      </header>
-      <MouseBall
-        show={showBall}
-        ballAnimation={false}
-        xStartProp={0}
-        yStartProp={0}
-      />
-      <motion.div className={styles.intro} style={{ y: transformTextY }}>
-        <Image src={work.logo} className={styles.logo} alt="Logo" priority />
-        <div className={styles.titleContainer}>
-          <motion.h1
-            className={classNames(styles.title, "gradientText")}
-            variants={textAnimation}
-            initial="initial"
-            animate="animate"
-          >
-            {work.title}
-          </motion.h1>
-        </div>
-        <div className={styles.subtitleContainer}>
-          <motion.h2
-            className={styles.subtitle}
-            variants={textAnimation}
-            initial="initial"
-            animate="animate"
-          >
-            {work.subtitle}
-          </motion.h2>
-        </div>
-      </motion.div>
-      <section>
-        <motion.p className={styles.overview} ref={containerTextRef}>
-          {texts.overview}
-        </motion.p>
-        <div className={styles.workInfo}>
-          {/* <h3>Stack</h3> */}
-          <h3 className={styles.smallTitle}>Role</h3>
-          <h3 className={styles.smallTitle}>Page</h3>
+    <>
+      <Head>
+        <title>{work.title} • Noah Guardiola</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>{" "}
+      <main className={styles.main}>
+        <header
+          className={classNames(intro.header)}
+          style={{ position: "fixed" }}
+        >
+          <a onClick={handleTransitionHome} style={{ color: headerColor }}>
+            Noah Guardiola
+          </a>
+        </header>
+        <MouseBall
+          show={showBall}
+          ballAnimation={false}
+          xStartProp={0}
+          yStartProp={0}
+        />
+        <motion.div className={styles.intro} style={{ y: transformTextY }}>
+          <Image src={work.logo} className={styles.logo} alt="Logo" priority />
+          <div className={styles.titleContainer}>
+            <motion.h1
+              className={classNames(styles.title, "gradientText")}
+              variants={textAnimation}
+              initial="initial"
+              animate="animate"
+            >
+              {work.title}
+            </motion.h1>
+          </div>
+          <div className={styles.subtitleContainer}>
+            <motion.h2
+              className={styles.subtitle}
+              variants={textAnimation}
+              initial="initial"
+              animate="animate"
+            >
+              {work.subtitle}
+            </motion.h2>
+          </div>
+        </motion.div>
+        <section>
+          <motion.p className={styles.overview} ref={containerTextRef}>
+            {texts.overview}
+          </motion.p>
+          <div className={styles.workInfo}>
+            {/* <h3>Stack</h3> */}
+            <h3 className={styles.smallTitle}>Role</h3>
+            <h3 className={styles.smallTitle}>Page</h3>
 
-          {/* <div className={styles.line}></div> */}
-          <div className={styles.line}></div>
-          <div className={styles.line}></div>
+            {/* <div className={styles.line}></div> */}
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
 
-          {/* <div className={styles.pills}>
+            {/* <div className={styles.pills}>
             <TechPill name="TypeScript" color="#3178C6" />
             <TechPill name="NodeJS" color="#68A063" />
             <TechPill name="MySQL" color="#00758F" />
             <TechPill name="AWS" color="#FF9900" />
           </div> */}
-          <div className={styles.info}>
-            <p>{texts.role}</p>
-          </div>
-          <div className={styles.info}>
-            <a href={`http://${texts.page}/`} target="_blank" rel="noopener">
-              {texts.page}
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.mockupContainer}>
-          <div className={styles.mockupLg}>
-            <Image src={mockupLg} className={styles.mockupLgImage} />
-            <div>
-              <video muted loop autoPlay>
-                <source src={video} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+            <div className={styles.info}>
+              <p>{texts.role}</p>
+            </div>
+            <div className={styles.info}>
+              <a href={`http://${texts.page}/`} target="_blank" rel="noopener">
+                {texts.page}
+              </a>
             </div>
           </div>
-        </div>
-        <div className={styles.workInfo}>
-          <h3 className={styles.smallTitle}>Why</h3>
-          <h3 className={styles.smallTitle}>Stack</h3>
 
-          <div className={styles.line}></div>
-          <div className={styles.line}></div>
+          <div className={styles.mockupContainer}>
+            <div className={styles.mockupLg}>
+              <Image src={mockupLg} className={styles.mockupLgImage} />
+              <div>
+                <video muted loop autoPlay>
+                  <source src={`/videos/${work.video}.mp4`} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
+          </div>
+          <div className={styles.workInfo}>
+            <h3 className={styles.smallTitle}>Why</h3>
+            <h3 className={styles.smallTitle}>Stack</h3>
 
-          <div className={styles.text}>
-            <p>{texts.why}</p>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+
+            <div className={styles.text}>
+              <p>{texts.why}</p>
+            </div>
+            <div className={styles.pills}>
+              {pills.map((pill) => (
+                <TechPill key={pill} name={pill} />
+              ))}
+            </div>
           </div>
-          <div className={styles.pills}>
-            {pills.map((pill) => (
-              <TechPill key={pill} name={pill} />
-            ))}
-          </div>
-        </div>
-        <motion.div className={styles.phoneMockups} ref={containerRef}>
-          <motion.div style={{ y: transformRightY }}></motion.div>
-          <motion.div style={{ y: transformLeftY }}></motion.div>
-        </motion.div>
-      </section>
-      <footer className={styles.footer} ref={footerRef}>
-        <div className={styles.nextCaseTitle}>
-          <h3 className={styles.smallTitle}>Next Case</h3>
-        </div>
-        <div
-          className={styles.nextCaseContainer}
-          onMouseEnter={() => setShowBall({ show: true, text: "Next" })}
-          onMouseLeave={() => setShowBall({ show: false, text: "" })}
-          onClick={handleTransitionOut}
-          style={{ marginTop: transitionIn ? "10vh" : "20vh" }}
-        >
-          <div className={styles.nextContainer}>
-            <div
-              className={classNames(styles.logoNextContainer, {
-                [styles.logoNextContainerTransition]: transitionIn,
-              })}
-            >
+          <motion.div className={styles.phoneMockups} ref={containerRef}>
+            {work.images?.uml ? (
               <Image
-                src={nextWork.logo}
-                className={styles.logoNext}
-                alt="Logo"
+                src={work.images.uml}
+                style={{
+                  height: "100%",
+                  width: "65%",
+                }}
               />
-            </div>
-            <div
-              className={styles.backgroundTransition}
-              style={{
-                width: transitionIn ? "150vw" : "6vw",
-                height: transitionIn ? "150vw" : "6vw",
-              }}
-            ></div>
+            ) : (
+              <>
+                <motion.div style={{ y: transformRightY }}>
+                  <Image
+                    src={work.images.screen1}
+                    style={{ objectFit: "cover", height: "100%" }}
+                  />
+                </motion.div>
+                <motion.div style={{ y: transformLeftY }}>
+                  <Image
+                    src={work.images.screen2}
+                    style={{ objectFit: "cover", height: "100%" }}
+                  />
+                </motion.div>
+              </>
+            )}
+          </motion.div>
+        </section>
         <footer className={styles.footer} ref={footerRef}>
           <div className={styles.nextCaseTitle}>
             <h3 className={styles.smallTitle}>Next Case</h3>
