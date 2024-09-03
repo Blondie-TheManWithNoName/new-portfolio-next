93182017965c44ae031c6820004822284d064456
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-09-03 22:48:27 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,27 @@ export default function App({ Component, pageProps }) {
 
   if (width <= height * 1.5) {
     return (
-      <div>
-        <p style={{ fontSize: "5vw" }}>
-          Page not ready to support this screen dimensions.
+      <div style={{ backgroundColor: "#1c1d20", height: "100vh" }}>
+        <p
+          style={{
+            fontSize: "5vw",
+            color: "white",
+            paddingInline: "10vw",
+            paddingTop: "10vh",
+          }}
+        >
+          Working on supporting this kind of screens... You can take a look at
+          my{" "}
+          <a
+            href="https://noahguardiola.me/"
+            style={{ color: "lightskyblue", textDecorationLine: "underline" }}
+          >
+            old portfolio
+          </a>{" "}
+          in the meantime :)
+        </p>
+        <p style={{ fontSize: "5vw", color: "white", paddingInline: "10vw" }}>
+          Thanks!
         </p>
       </div>
     );
