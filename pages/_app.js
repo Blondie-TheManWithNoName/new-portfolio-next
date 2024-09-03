ae963e8d994767988f37fa409a90d99b7965914f
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-09-03 22:29:47 +0200

    message for unsupported screens

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,19 @@
+//
+import useWindowSize from "../hooks/useWIndowSize";
 import "../styles/global.css";
+
 export default function App({ Component, pageProps }) {
+  const { width, height } = useWindowSize();
+
+  if (width <= height * 1.5) {
+    return (
+      <div>
+        <p style={{ fontSize: "5vw" }}>
+          Page not ready to support this screen dimensions.
+        </p>
+      </div>
+    );
+  }
+
   return <Component {...pageProps} />;
 }
